f5453545ee47431fcbde05ead4de1b1163e86522
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2025-02-20 22:56:55 +0530

    Riskprofiling API initialization

---
+++ src/test/java/com/easebank/irpkne/IrpkneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easebank.irpkne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrpkneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
